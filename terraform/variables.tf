633f2bc93827994d2b5da0aab689e29e271a11fd
Arshad Aslam <arshad_aslam@outlook.com>
2025-08-22 04:56:49 +0100

    Update project with Terraform config and final HTML/CSS

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,8 +1,22 @@
-variable "github_token" {
-  type      = string
-  sensitive = true
+variable "github_owner" {
+  description = "Your GitHub username"
+  type        = string
 }
 
-variable "github_owner" {
-  type = string
+variable "github_token" {
+  description = "Your GitHub Personal Access Token (PAT)"
+  type        = string
+  sensitive   = true
 }
+
+variable "repository_name" {
+  description = "The name of your GitHub repository"
+  type        = string
+}
+
+variable "pages_branch" {
+  description = "The branch that will serve GitHub Pages"
+  type        = string
+  default     = "main"
+}
+
